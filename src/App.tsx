d0a6c8d2f4b648639ddf9a4a595f9ecdefb38857
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-18 00:21:04 -0600

    task: jsdoc base config, todo: test output, consider typedoc

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Main from './components/Main'
 import TestCard from './components/TestCard'
-
+/**
+ * Documentatio re App
+ * @returns app child components ({@link Main} and {@link TestCard}) wrapped in {@link BrowserRouter}
+ */
 export function App() {
   return (
     <BrowserRouter>
