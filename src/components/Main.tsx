fa938b714a16aaa7c16969bb33795bfc2854f68a
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-18 00:21:04 -0600

    task: jsdoc base config, todo: test output, consider typedoc

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 type Props = {
   children?: JSX.Element | JSX.Element[]
 }
+/**
+ * Stand-in component for foundation or style provider
+ * @returns children
+ */
 
 function Main({ children }: Props) {
   return <div>{children}</div>
